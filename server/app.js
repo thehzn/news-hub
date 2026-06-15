b61653d56cf892335b186fd2974f59098c0175d2
thehzn <107251759+thehzn@users.noreply.github.com>
2026-06-15 09:07:37 +0530

    fix request logger middleware

--- server/app.js
+++ server/app.js
@@ -1,39 +1,53 @@
-const express = require("express");
-const cors = require("cors");
+// const express = require("express");
+// const cors = require("cors");
 
-const app = express();
-const cookieParse = require("cookie-parser");
+// const app = express();
+// const cookieParse = require("cookie-parser");
 
-//middlewares
-app.use(cors({
+// //middlewares
+// app.use(cors({
 
-    origin:[ 'http://localhost:5173',"https://news-hub-green-kappa.vercel.app"] ,
-    credentials: true,              
-    methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}
-));
-app.use(express.json());
+//     origin:[ 'http://localhost:5173',"https://news-hub-green-kappa.vercel.app"] ,
+//     credentials: true,              
+//     methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
+//     allowedHeaders: ['Content-Type', 'Authorization']
+// }
+// ));
+// app.use(express.json());
+// app.use(cookieParse());
+// app.use(express.urlencoded({ extended: true }));
+
+
+// const adminRoutes=require("./routers/adminRoutes");
+// const newsRoutes=require("./routers/newsRoutes");
+// app.use("/api/admin",adminRoutes);
+// app.use("/api/news",newsRoutes);
+
+
+
+
+// app.use((req, res) => {
+//   console.log(`Incoming Request: ${req.method} ${req.url}`);
+//   res.status(404).json({ message: "Route not found" });
+// });
+
+// app.use((err, req, res, next) => {
+//   console.error(err.stack);
+//   res.status(500).json({ message: err.message || "Internal server error" });
+// });
+
+// module.exports = app;
+
 app.use(cookieParse());
 app.use(express.urlencoded({ extended: true }));
 
-
-const adminRoutes=require("./routers/adminRoutes");
-const newsRoutes=require("./routers/newsRoutes");
-app.use("/api/admin",adminRoutes);
-app.use("/api/news",newsRoutes);
-
-
-
-
-app.use((req, res) => {
+// Move logger here with next()
+app.use((req, res, next) => {
   console.log(`Incoming Request: ${req.method} ${req.url}`);
-  res.status(404).json({ message: "Route not found" });
+  next();
 });
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: err.message || "Internal server error" });
-});
-
-module.exports = app;
+const adminRoutes = require("./routers/adminRoutes");
+const newsRoutes = require("./routers/newsRoutes");
+app.use("/api/admin", adminRoutes);
+app.use("/api/news", newsRoutes);
